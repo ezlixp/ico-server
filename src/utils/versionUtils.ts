8ac07961ad0530e0e1117759ae664bfa8648dbfd
ezlixp <henrygoals2007@gmail.com>
2026-04-01 19:09:00 -0400

    fix: minimum mod version

--- src/utils/versionUtils.ts
+++ src/utils/versionUtils.ts
@@ -2,7 +2,7 @@
  * Defines the minimum allowed mod version in the format:
  * [major, minor, revision, beta]
  */
-const minimumModVersion = [1, 1, 0];
+const minimumModVersion = [1, 1, 0, 500];
 const versionExtractorPattern = new RegExp(
     "guildapi/(?<major>\\d+).(?<minor>\\d+).(?<revision>\\d+)(-beta.(?<beta>\\d+))?",
 );
